aacc971b56e85eb4ac3e2e63abc9670f6d2716b5
Chintan Patel <108448357+chintan0207@users.noreply.github.com>
2025-08-19 15:08:14 +0530

    Update API_URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8081/api";
+export const API_URL = "https://festivalfund-backend.onrender.com";
 
 export const colors = [
   "bg-gradient-to-r from-green-500 to-emerald-500",
